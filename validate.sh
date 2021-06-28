21b2e31bb12c0db1c570723b0327d8fce11c92ab
Abby Carey <abigailcarey123@gmail.com>
2021-06-28 10:03:50 -0700

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 TMP_DIR=$(mktemp -d)
-kpt fn source -o unwrap ../k8s/ > ${TMP_DIR}/manifests.yaml
-cp ../validate/policy.yaml ${TMP_DIR}/policy.yaml
+kpt fn source -o unwrap ./k8s/ > ${TMP_DIR}/manifests.yaml
+cp ./policy.yaml ${TMP_DIR}/policy.yaml
 kpt fn eval --image $1 --image-pull-policy never ${TMP_DIR}
